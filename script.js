1ad1bcb97babae6398eac9399908ccc59bf9f4e6
Ravi varshney <80498450+varshneyrav@users.noreply.github.com>
2023-08-15 14:06:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,156 +1,93 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta content="origin" name="referrer">
-    <!-- <meta content="dark" name="color-scheme"> -->
-    <link rel="icon" type="image/x-icon" href="./images/favicon.ico">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!--     <script src="script.js" type="text/javascript"></script> -->
-    <title> One Stop | News</title>
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
-    <div id="preloaders">
-    </div>
+const API_KEY1 = "628881c4bc3a9d2c6892dc928783469c";
+// const API_KEY1 = "4ca2748c39c15c160959ebf252224d66";
+const url_ = "https://gnews.io/api/v4/search?q=";
 
-    <nav>
-        <div class="main-nav container flex">
-            <a href="#" onclick="reload()" class="company-logo">
-                <img src="./images/logo.png" alt="company logo">
-            </a>
+window.addEventListener("load", () => fetchNews("India"));
 
-            <body onload=display_ct();>
-                <span id='ct5'></span>
-                <h3 class="name">ONE STOP NEWS</h3>
-                <div class="search-bar flex">
-                    <input id="search-text" type="text" class="news-input" placeholder="e.g. India">
-                    <button id="search-button" class="search-button" onclick="onNewsSearch('fullscreen')">Search</button>
-                </div>
-        </div>
-    </nav>
-    <!--            second nav           -->
-    <div class="items">
-        <div class="nav-links">
-            <ul class="flex">
-                <li class="hover-link nav-item" id="Latest News" onclick="onNavItemClick('Latest News')">Latest News
-                </li>
-                <li class="hover-link nav-item" id="technology" onclick="onNavItemClick('technology')">Technology</li>
-                <li class="hover-link nav-item" id="stocks" onclick="onNavItemClick('stocks')">Stocks</li>
-                <li class="hover-link nav-item" id="Indian finance" onclick="onNavItemClick('Indian finance')">Finance
-                </li>
-                <li class="hover-link nav-item" id="politics" onclick="onNavItemClick('politics')">Politics</li>
-                <li class="hover-link nav-item" id="world" onclick="onNavItemClick('world')">world</li>
-                <li class="hover-link nav-item" id="Entertainment" onclick="onNavItemClick('Entertainment')">
-                    Entertainment</li>
-                <li class="hover-link nav-item" id="Indian Astrology" onclick="onNavItemClick('Indian Astrology')">
-                    Astrology</li>
-                <li class="hover-link nav-item" id="Indian Shop now" onclick="onNavItemClick('Indian Shop now')">Shop
-                    now</li>
-                <li class="hover-link nav-item" id="Life Style" onclick="onNavItemClick('Life Style')">Life Style</li>
-                <li class="hover-link nav-item" id="cricket" onclick="onNavItemClick('stocks')">Stocks</li>
+function reload() {
+    window.location.reload();
+}
 
-            </ul>
-        </div>
-    </div>
+let app_loader = document.getElementById('ons_loader');
+
+async function fetchNews(query) {
+    app_loader.classList.remove("display_none");
+    app_loader.classList.add("display_grid");
+    // let apiEndpoint = './news.json';
+    let apiEndpoint = `${url_}${query}&apikey=${API_KEY1}`;
+    const res = await fetch(apiEndpoint);
+    const data = await res.json();
+    console.log('response', data);
+    if(data && data.articles && data.articles.length ){
+	// below code for make unique records
+        // var mapNews = new Map();
+        // for (let singleNews of data.data) {
+        //     mapNews.set(singleNews["title"], singleNews);
+        // }
+        // var iteratorValues = mapNews.values();
+        // var uniqueRecords = [...iteratorValues];
+        // console.log('uniqueData', uniqueRecords);
+        bindData(data.articles);
+    } else {
+        alert(`Opps! ${keyWord} news are not available!`)
+    }
     
-    <!-- <div class="weathers">
-        <form>
-            <div class="row" id="heading">
-                <pre class="trend">[ Weather news ]</pre>
-                <input type="search" id="search" placeholder="eg. Agra " />
-            </div>
+}
 
-        </form>
-        <div class="row" id="weather">
-        </div>
-    </div> -->
+function bindData(articles) {
+    const cardsContainer = document.getElementById("cards-container");
+    const newsCardTemplate = document.getElementById("template-news-card");
 
-    <!-- <div class="trending-news">
-        <pre class="trend">[ trending news ]</pre><br>
-        <div class="items_">
-            <ul class="ul">
-                <li class="hover-link nav-item" id="BOLLYWOOD" onclick="onNavItemClick('BOLLYWOOD')">BOLLYWOOD</li>
-                <li class="hover-link nav-item" id="weather News" onclick="onNavItemClick('weather News')">weather News
-                </li>
-                <li class="hover-link nav-item" id="ICC WORLD CUP 2023" onclick="onNavItemClick('ICC WORLD CUP 2023')">
-                    ICC WORLD CUP 2023</li>
-                <li class="hover-link nav-item" id="RAIN NEWS OF INDIA" onclick="onNavItemClick('RAIN NEWS OF INDIA')">
-                    RAIN NEWS TODAY</li>
-                <li class="hover-link nav-item" id="Science" onclick="onNavItemClick('Science')">Science</li>
-            </ul>
-        </div>
-    </div> -->
-    <div class="small_screen_search_row">
-        <div class="search-bar none flex">
-            <input id="search-input-mobile" type="text" class="news-input" placeholder="e.g. India">
-            <button id="search-button" class="search-button" onclick="onNewsSearch('mobile')">Search</button>
-        </div>
-    </div>
-    <main>
-        <div class="cards-container container flex" id="cards-container">
-        </div>
-    </main>
-    <template id="template-news-card">
-        <div class="card">
-            <div class="card-header">
-                <img src="https://via.placeholder.com/400x200" alt="news-image" id="news-img">
-            </div>
-            <div class="card-content">
-                <h2 id="news-title">This is the Title</h2>
-                <h6 class="news-source" id="news-source">End Gadget 26/08/2023</h6>
-                <p class="news-desc" id="news-desc">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae
-                    saepe quis voluptatum quisquam vitae doloremque facilis molestias quae ratione cumque!</p>
-            </div>
-        </div>
-    </template>
-    <!-- <div id="footer">copyright (c)</div> -->
-    <footer id="footer">
-        <div class="container">
-            <div class="footerTop">
-                <div class="footLogo">
-                    <a class="fotLogo" href="#"> Explore the: One Stop News</a><br>
-                    <!-- <img src="Logo copy.png" alt="company logo"> -->
-                    <hr>
-                    <div class="copyright"> Copyright © 2023 OSN. The OSN is not responsible for the content of external
-                        sites. All Right Reserved </div>
-                </div>
-    </footer>
+    cardsContainer.innerHTML = "";
 
-    <div class="ons_loader" id="ons_loader">
-        <img src="logo.png" />
-    </div>
+    articles.forEach((article) => {
+        // if (!article.image) return;
+        const cardClone = newsCardTemplate.content.cloneNode(true);
+        fillDataInCard(cardClone, article);
+        cardsContainer.appendChild(cardClone);
+    });
+    app_loader.classList.remove("display_grid");
+    app_loader.classList.add("display_none");
+}
 
-    <!-- <script src="load.js"></script> -->
-    <!-- <script src="w_app.js"></script>
-<script src="script.js"></script> -->
+function fillDataInCard(cardClone, article) {
+    const newsImg = cardClone.querySelector("#news-img");
+    const newsTitle = cardClone.querySelector("#news-title");
+    const newsSource = cardClone.querySelector("#news-source");
+    const newsDesc = cardClone.querySelector("#news-desc");
 
-</body>
-<script>
-    var loading = document.getElementById("preloaders");
-    window.addEventListener("load", function () {
-        <!-- loading.style.animationDelay="2s"; -->
-        loading.style.display = "none";
-    })
-</script>
-<script type="text/javascript">
-    function display_ct5() {
-        var x = new Date()
-        var ampm = x.getHours() >= 12 ? ' PM' : ' AM';
+    newsImg.src = article.image;
+    newsTitle.innerHTML = article.title;
+    newsDesc.innerHTML = article.description;
 
-        var x1 = x.getMonth() + 1 + "/" + x.getDate() + "/" + x.getFullYear();
-        x1 = x1 + " \n " + x.getHours() + ":" + x.getMinutes() + ":" + x.getSeconds();
-        document.getElementById('ct5').innerHTML = x1;
-        display_c5();
-    }
-    function display_c5() {
-        var refresh = 1000; // Refresh rate in milli seconds
-        mytime = setTimeout('display_ct5()', refresh)
-    }
-    display_c5()
-</script>
-<script src="script.js" type="text/javascript"></script>
-<!-- <script src="w_app.js" type="text/javascript"></script> -->
+    const date = new Date(article.publishedAt).toLocaleString("en-US", {
+        timeZone: "Asia/Jakarta",
+    });
 
-</html>
+    newsSource.innerHTML = `${article.source.name} · ${date}`;
+
+    cardClone.firstElementChild.addEventListener("click", () => {
+        window.open(article.url, "_blank");
+    });
+}
+
+let curSelectedNav = null;
+function onNavItemClick(id) {
+    fetchNews(id);
+    const navItem = document.getElementById(id);
+    curSelectedNav?.classList.remove("active");
+    curSelectedNav = navItem;
+    curSelectedNav.classList.add("active");
+}
+
+const searchButton = document.getElementById("search-button");
+const searchText = document.getElementById("search-text");
+const mobileSearchText = document.getElementById("search-input-mobile");
+
+function onNewsSearch(source) {
+    let query = (source === 'mobile') ? mobileSearchText.value : searchText.value;
+    if (!query) return;
+    fetchNews(query);
+    curSelectedNav?.classList.remove("active");
+    curSelectedNav = null;
+}
