7d6470238279922a3624f9b91d13355b5fe8442d
varshneyrav <varshneyravi49@gmail.com>
2023-08-17 20:48:15 +0530

    update

--- w_app.js
+++ w_app.js
@@ -1,5 +1,5 @@
 const API_KEY = `3265874a2c77ae4a04bb96236a642d2f`
-const form = document.querySelector("form")
+const form = document.querySelector(".weather_dia form")
 const search = document.querySelector("#search")
 const weather = document.querySelector("#weather")
 
